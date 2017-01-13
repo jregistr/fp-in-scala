5f2d34fdfaebac367f2eabe8d542dde4058751e5
Jeff Registre <jregistr@oswego.edu>
2017-01-13 13:46:12 -0500

    more

--- src/main/scala/Funcs.scala
+++ src/main/scala/Funcs.scala
@@ -2,24 +2,33 @@ import scala.annotation.tailrec
 
 object Funcs {
 
-  def getFirst[T](array:Array[T], p:(T) => Boolean) : Option[T] = {
-    @tailrec def loop(i:Int):Option[T] = {
-      if(i >= array.length) None
+  def getFirst[T](array: Array[T], p: (T) => Boolean): Option[T] = {
+    @tailrec def loop(i: Int): Option[T] = {
+      if (i >= array.length) None
       else {
         val t = array(i)
         if (p(t)) Some(t)
         else loop(i + 1)
       }
     }
+
     loop(0)
   }
 
-  def isSorted[T](array: Array[T], compare:(T,T) => Boolean):Boolean = {
-    def loop(i:Int):Boolean = {
-      if(i + 1 >= array.length) true
-      else compare(array(i), array(i + 1)) & loop(i + 1)
+  def isSorted[T](array: Array[T], compare: (T, T) => Boolean): Boolean = {
+    def traverse(i: Int): Boolean = {
+      if (i + 1 >= array.length) true
+      else compare(array(i), array(i + 1)) & traverse(i + 1)
     }
-    loop(0)
+    traverse(0)
   }
+
+  def partial1[A, B, C](a: A, f: (A, B) => C): (B) => C = (b: B) => f(a, b)
+
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) = a => b => f(a, b)
+
+  def unCurry[A, B, C](f: A => B => C): (A, B) => C = (a, b) => f(a)(b)
+
+  def compose[A, B, C](f: B => C, g: A => B): A => C = a => f(g(a))
 
 }
--- src/test/scala/FuncsSpec.scala
+++ src/test/scala/FuncsSpec.scala
@@ -6,15 +6,21 @@ class FuncsSpec extends FlatSpec with Matchers {
 
   case class Car(name: String, price: Int)
 
-  def makeCars = Array(
+  val cars = Array(
     Car("Camry", 100),
     Car("CRV", 300),
     Car("Camry", 500),
     Car("CX-3", 2000)
   )
 
+  val people = Map(
+    "Jeff" -> 5000,
+    "Jack" -> 1000,
+    "Peter" -> 50,
+    "Bob" -> 400
+  )
+
   it should "Return some value from the array" in {
-    val cars = makeCars
     val search = Funcs.getFirst(cars, (car: Car) => car.name == "Camry" && car.price == 500)
     search.isDefined should be(true)
     Inside.inside(search.get) { case Car(name, price) =>
@@ -37,6 +43,29 @@ class FuncsSpec extends FlatSpec with Matchers {
 
   it should "Say it is not sorted" in {
     Funcs.isSorted(Array(1, 3, 2, 4, 5, 6), (a: Int, b: Int) => b >= a) should be(false)
+  }
+
+  it should "make some delicious curry" in {
+    val stringFormat = "%s has a nice %s that costs %d green backs"
+    val camry = cars(0)
+    val curry = Funcs.curry((car:Car, owner:String) => stringFormat.format(owner, car.name, car.price))
+    val formatFunc: (String) => String = curry(camry)
+    val formatted = formatFunc("Jeff")
+    println(formatted)
+    formatted should be (stringFormat.format("Jeff", camry.name, camry.price))
+  }
+
+  it should "make a very nice composed function" in {
+    // A:Person, B:Int(Budget), C:Cars
+    def canBuy:Int => List[Car] = budget => cars.filter(_.price <= budget).toList
+    def walletOf:String => Int = person => people(person)
+
+    val carsForPerson: (String) => List[Car] = Funcs.compose(canBuy, walletOf)
+    println(carsForPerson("Jeff"))
+    println(carsForPerson("Jack"))
+    println(carsForPerson("Peter"))
+    println(carsForPerson("Bob"))
+
   }
 
 }
